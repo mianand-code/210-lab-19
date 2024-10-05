5d77b89a58f3fc8cf7f79295da0c19df2f962d83
Mira Anand <miraanand@comcast.net>
2024-10-05 12:57:24 -0700

    10 minute update - 1

--- Lab19MiraAnand.cpp
+++ Lab19MiraAnand.cpp
@@ -2,7 +2,29 @@
 // Module 6, Lesson: Linked Lists, Assignment: Abstract & Automate
 // IDE used: Visual Studio Code for Mac
 
+#include <cstdlib> // needed to generate random number
+#include <ctime> // needed to generate random number
+#include <fstream> // to be able to read data from an external .txt file
+#include <iomanip> // to use setprecision()
+#include <iostream>
+#include <string>
+#include <vector> // to use STD::vector, my container of choice for this assignment
+using namespace std;
+
+// creation of Movie Class
+class Movie
+{
+    // private member variables
+    private:
+        struct ReviewNode // creation of a struct named "Review", 
+        {
+
+        };
+};
+
 int main()
 {
+    srand(time(0)); // needed as the first line in main() to generate random numbers for movie rating
     
+    return 0;
 }
